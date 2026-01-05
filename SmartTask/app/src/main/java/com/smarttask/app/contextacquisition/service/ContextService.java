32e4332c4b2e8416d1ee19d676779686a7a55567
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2026-01-05 16:57:19 +0530

    delete contextacquisition\service\ContextService.java

--- SmartTask/app/src/main/java/com/smarttask/app/contextacquisition/service/ContextService.java
+++
@@ -1,6 +0,0 @@
-// contextacquisition/service/ContextService.java
-package com.smarttask.app.contextacquisition.service;
-
-public class ContextService {
-    // TODO: implement background context collection
-}
